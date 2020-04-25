2a257103f69dbe9b7c1d88d3a1eb31a4a5756da1
ikepggthb <61868325+ikepggthb@users.noreply.github.com>
2020-04-26 00:51:43 +0900

    beta1
    Added a function to notify mail by the notification function (toast) of windows10."

--- uumail.py
+++ uumail.py
@@ -2,21 +2,21 @@ import requests
 import time
 import re
 import datetime
-#import threading
-#import wx
+from plyer import notification
+from getpass import getpass
+
 
 # -*- coding: utf-8 -*-
 
-ID = ""
-PASSWD = ""
+ID = input('Input your ID: ')
+PASSWD = getpass('Input your Password: ')
 URL = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin  "
 URL_LOGIN = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin/login_auth"
 URL_LOGOUT = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin/logout"
-OFTEN = 60 * 60 * 1
+OFTEN = 60 * 60 * 1 # 通知を確認する頻度(秒)
 LOGIN_DATA = {
     'login_page_lang': 'ja',
     'charset': 'UTF-8',
-    'http_user_agent': 'value="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"',
     'am_authid': ID,
     'am_authpasswd': PASSWD,
     'language': 'auto',
@@ -36,82 +36,69 @@ MESSAGE_ERROR = (
 def get_info():
     # セッションの作成
     session = requests.session()
-    response = session.get(URL)
+    try:
+        response = session.get(URL, timeout=(3.0, 8))
+        # ログインurlにdataをPOST
+        login = session.post(URL_LOGIN, data=LOGIN_DATA, timeout=(3.0, 8))
+        # ログイン後のソースを取得
+        login_redirect_code = login.text
 
-    # ログインurlにdataをPOST
-    login = session.post(URL_LOGIN, data=LOGIN_DATA)
-    if login.status_code != 200:
+        # 取得したコードのJavascript内にある"self.location.href="のあとのURLを取得
+        temp = re.search('self.location.href=\".*?\"', login_redirect_code)
+        redirect_url = login_redirect_code[temp.start() + 20: temp.end() - 1]
+
+        # リダイレクト
+        session.get(redirect_url, timeout=(3.0, 8))
+    except :
         return FAIL_ACSESS
 
-    # ログイン後のソースを取得
-    login_redirect_code = login.text
+    try:
+        # urlからセッションIDを取得
+        session_id = re.search("[0-9].*", redirect_url)
+        session_id = session_id.group()
+    except :
+        return WRONG_DATA
 
-    # 取得したコードのJavascript内にある"self.location.href="のあとのURLを取得
-    temp = re.search('self.location.href=\".*?\"', login_redirect_code)
-    if temp is None:
-        return FAIL_ACSESS
-    redirect_url = login_redirect_code[temp.start() + 20: temp.end() - 1]
-
-    # リダイレクト
-    session.get(redirect_url)
-
-    # urlからセッションIDを取得
-    session_id = re.search("[0-9].*", redirect_url)
-    if session_id is None:
-        return WRONG_DATA  # ここで間違った場合、IDやパスワードが間違っている事が多い
-    session_id = session_id.group()
-
-    time.sleep(1)
-
-    # homeへアクセス
-    page = session.get(redirect_url.replace("top", "home"))
-    if page.status_code != 200:
+    try:
+        # homeへアクセス
+        page = session.get(redirect_url.replace("top", "home"))
+        # 新着メール情報
+        mail_info_code = re.search("<div class=\"mail_recent shadow\">(.|\s)*?</div>", page.text)
+        mail_info_code = mail_info_code.group()
+        mail_info_code = mail_info_code.replace("\n", "")
+        mail_info_code = mail_info_code.replace(" ", "")
+        mail_info = re.search("<li>.*?</li>", mail_info_code)
+        mail_info = mail_info.group()[4:-5]
+    except:
         return FAIL_GET_INFO
-
-    # 新着メール情報
-    mail_info_code = re.search("<div class=\"mail_recent shadow\">(.|\s)*?</div>", page.text)
-    if mail_info_code is None:
-        return FAIL_GET_INFO
-    mail_info_code = mail_info_code.group()
-    mail_info_code = mail_info_code.replace("\n", "")
-    mail_info_code = mail_info_code.replace(" ", "")
-    mail_info = re.search("<li>.*?</li>", mail_info_code)
-    if mail_info is None:
-        return FAIL_GET_INFO
-    mail_info = mail_info.group()[4:-5]
-
-    time.sleep(1)
 
     # logout処理
     logout_url = URL_LOGOUT + "?id=" + session_id
-    logout = session.get(logout_url)
+    logout = session.get(logout_url, timeout=(3.0, 8))
 
     return mail_info
 
 def uumail():
     i = 0
-    while True:
+    while i < 5:
         result_get_info = get_info()
         dt_now = datetime.datetime.now()
+        dt_now_str = dt_now.strftime('%H:%M')
+        dt_now_str_all = dt_now.strftime('%Y年%m月%d日 %H:%M:%S')
 
-        print(dt_now.strftime('%Y年%m月%d日 %H:%M:%S'), ":", end="")
-
-        success_get_info = type(result_get_info) is str
-        if success_get_info:
-            print(result_get_info)
-            break
-
-        MESSAGE_ERROR[result_get_info]
-
+        if type(result_get_info) is str: # もし、get_info関数がエラーならば、その返り値は整数型
+            print(dt_now_str_all, ":",result_get_info)
+            notification.notify(title='uumail',message=dt_now_str+"\n"+result_get_info,app_name='UUmail通知')
+            return
+        else : # get_info関数がエラー
+            print(dt_now_str_all, ":",MESSAGE_ERROR[result_get_info])
+            notification.notify(title='uumail',message=dt_now_str+"\n"+MESSAGE_ERROR[result_get_info],app_name='UUmail通知')
         i += 1
-        if i >= 5:
-            print("5回失敗しました")
-            break
-
         time.sleep(3)
+    print("5回失敗しました")
 
-i = 0
 time_start = time.time()
+i = 0
 while True:
     timer = time.time()
     if timer >= time_start + OFTEN * i:
