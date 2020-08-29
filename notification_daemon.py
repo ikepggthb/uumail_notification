70683c92b27cbc02883cd00c747e012a034010bf
ikepggthb <61868325+ikepggthb@users.noreply.github.com>
2020-08-30 00:55:31 +0900

    Update notification_daemon.py
    use class

--- notification_daemon.py
+++ notification_daemon.py
@@ -26,127 +26,110 @@ import win32api
 import win32.lib.win32con as win32con
 
 import toast
-from setting import passcrypt
-from setting import umn_config
-
-CONFIG = umn_config.read_config()
-
-# アカウントデータ取得
-try:
-    ACCOUNT_DATA = passcrypt.read_data()
-except:
-    q = win32api.MessageBox(0, u"アカウント情報の取得に失敗しました\nアカウントを設定しますか", u"uumail notification - エラー", win32con.MB_YESNO | win32con.MB_ICONERROR)
-    if q == win32con.IDYES:
-        umn_config.open_setting()
-    win32api.MessageBox(0, u"終了します。", u"uumail notification - エラー", win32con.MB_OK | win32con.MB_ICONERROR)
-    sys.exit(1)
-
-ID = ACCOUNT_DATA[0]
-PASSWD = ACCOUNT_DATA[1]
-SYNC_INTERVAL = 60 * int(CONFIG['sync_interval']) # 秒
-URL = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin  "
-URL_LOGIN = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin/login_auth"
-URL_LOGOUT = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin/logout"
-LOGIN_DATA = {
-    'login_page_lang': 'ja',
-    'charset': 'UTF-8',
-    'am_authid': ID,
-    'am_authpasswd': PASSWD,
-    'language': 'auto',
-    'ajax': 'on'
-}
-
-ERROR = {
-    "FAIL_ACSESS"   : 1,
-    "WRONG_DATA"    : 2,
-    "FAIL_GET_INFO" : 3
-}
-MESSAGE_ERROR = (
-    "正常終了",
-    "アクセスに失敗しました",
-    "IDやパスワードが違う可能性があります",
-    "情報の取得に失敗しました"
-)
-
-def get_info():
-    # セッションの作成
-    session = requests.session()
-    try:
-        login = session.post(URL_LOGIN, data=LOGIN_DATA, timeout=(3.0, 8))
-        # ログイン後のソースを取得
-        login_redirect_code = login.text
-
-        # 取得したコードのJavascript内にある"self.location.href="のあとのURLを取得
-        temp = re.search('self.location.href=\".*?\"', login_redirect_code)
-        redirect_url = login_redirect_code[temp.start() + 20: temp.end() - 1]
-
-        time.sleep(3)
-
-        # リダイレクト
-        session.get(redirect_url, timeout=(3.0, 8))
-    except :
-        return ERROR["FAIL_ACSESS"]
-
-    try:
-        # urlからセッションIDを取得
-        session_id = re.search("[0-9].*", redirect_url)
-        session_id = session_id.group()
-    except :
-        return ERROR["WRONG_DATA"]
-
-    try:
-        # homeへアクセス
-        time.sleep(3)
-        page = session.get(redirect_url.replace("top", "home"))
-        # 新着メール情報
-        mail_info_code = re.search("<div class=\"mail_recent shadow\">(.|\s)*?</div>", page.text)
-        mail_info_code = mail_info_code.group()
-        mail_info_code = mail_info_code.replace("\n", "").replace(" ", "")
-        mail_info = re.search("<li>.*?</li>", mail_info_code)
-        mail_info = mail_info.group()[4:-5]
-    except:
-        return ERROR["FAIL_GET_INFO"]
-
-    try:
-        # logout処理
-        logout_url = URL_LOGOUT + "?id=" + session_id
-        time.sleep(5)
-        logout = session.get(logout_url, timeout=(3.0, 8))
-    except:
-        return mail_info
-
-    return mail_info
 
 def notification(content):
-    dt_now = datetime.datetime.now()
-    dt_now_str = dt_now.strftime('%H:%M')
-    dt_now_str_all = dt_now.strftime('%Y年%m月%d日 %H:%M:%S')
-    # print(dt_now_str_all, ":",content)
     toast.toast("uumail", content)
 
+class Regularly_notify(threading.Thread):
+    def __init__(self,uumail_id,uumail_pass,sync_interval):
+        super(Regularly_notify, self).__init__()
+        self.id = uumail_id
+        self.passwd = uumail_pass
+        self.interval = sync_interval
+        self.url = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin  "
+        self.url_login = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin/login_auth"
+        self.url_logout = "https://uumail.cc.utsunomiya-u.ac.jp/am_bin/amlogin/logout"
+        self.login_data = {
+            'login_page_lang': 'ja',
+            'charset': 'UTF-8',
+            'am_authid': self.id,
+            'am_authpasswd': self.passwd,
+            'language': 'auto',
+            'ajax': 'on'
+        }
+        self.message = {
+            "FAIL_ACSESS"   : "アクセスに失敗しました",
+            "WRONG_DATA"    : "IDやパスワードが違う可能性があります",
+            "FAIL_GET_INFO" : "情報の取得に失敗しました"
+        }
+        self.last_info = None
+        self.setDaemon(True)
+    def get(self):
+        # セッションの作成
+        session = requests.session()
+        try:
+            login = session.post(self.url_login, data=self.login_data, timeout=(3.0, 8))
+            # ログイン後のソースを取得
+            login_redirect_code = login.text
 
-def notify_uumail(result_get_info_last):
-    i = 0
-    while i < 5:
-        result_get_info = get_info()
-        if type(result_get_info) is str:    # 取得成功したならば
-            if result_get_info != result_get_info_last:
-                notification(result_get_info)
-            return result_get_info
-        i += 1
-        time.sleep(5)
-    # print("5回失敗しました")
-    notification(MESSAGE_ERROR[result_get_info])
-    return result_get_info
+            # 取得したコードのJavascript内にある"self.location.href="のあとのURLを取得
+            temp = re.search('self.location.href=\".*?\"', login_redirect_code)
+            redirect_url = login_redirect_code[temp.start() + 20: temp.end() - 1]
 
-def notify_daemon():
-    time_start = time.time()
-    i = 0
-    last_info = None
-    notification("タスクトレイに常駐します。")
-    while True:
-        timer = time.time()
-        if timer >= time_start + SYNC_INTERVAL * i:
-            last_info = notify_uumail(last_info)
+            time.sleep(3)
+
+            # リダイレクト
+            session.get(redirect_url, timeout=(3.0, 8))
+        except :
+            self.obt_info = self.message["FAIL_ACSESS"]
+            return False
+
+        try:
+            # urlからセッションIDを取得
+            session_id = re.search("[0-9].*", redirect_url)
+            session_id = session_id.group()
+        except :
+            self.obt_info = self.message["WRONG_DATA"]
+            return False
+
+        try:
+            # homeへアクセス
+            time.sleep(3)
+            page = session.get(redirect_url.replace("top", "home"))
+            # 新着メール情報
+            mail_info_code = re.search("<div class=\"mail_recent shadow\">(.|\s)*?</div>", page.text)
+            mail_info_code = mail_info_code.group()
+            mail_info_code = mail_info_code.replace("\n", "").replace(" ", "")
+            mail_info = re.search("<li>.*?</li>", mail_info_code)
+            mail_info = mail_info.group()[4:-5]
+        except:
+            self.obt_info = self.message["FAIL_GET_INFO"] 
+            return False
+
+        try:
+            # logout処理
+            logout_url = self.url_logout + "?id=" + session_id
+            time.sleep(5)
+            logout = session.get(logout_url, timeout=(3.0, 8))
+        except:
+            self.obt_info = mail_info
+            return True
+
+        self.obt_info = mail_info
+        return True
+
+    def notify_uumail(self):
+        i = 0
+        while i < 5:
+            if self.get():
+                if self.obt_info != self.last_info:
+                    notification(self.obt_info)
+                    self.last_info = self.obt_info
+                return True
             i += 1
-        time.sleep(10)
+            time.sleep(5)
+        # 5回失敗
+        notification(self.obt_info)
+        return False
+
+    def run(self):
+        time_start = time.time()
+        i = 0
+        notification("タスクトレイに常駐します。")
+        while True:
+            timer = time.time()
+            if timer >= time_start + self.interval * i:
+                self.notify_uumail()
+                i += 1
+            time.sleep(10)
+
