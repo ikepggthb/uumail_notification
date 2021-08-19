30648aa219b793ee1e34abba50ea613a11296623
ikepggthb <ikkei1188@gmail.com>
2021-08-19 20:19:11 +0900

    copying

--- uumail_notification.py
+++ uumail_notification.py
@@ -1,3 +1,23 @@
+# uumail notification
+#
+# © 2020 Ikkei Yamada All Rights Reserved.
+# Twitter: @idkaeti
+# Email  : ikeprg@gmail.com
+
+#   Released under the GPLv3 license.
+#
+#   "uumail_notification" is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   "uumail_notification" is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with "uumail_notification".  If not, see <http://www.gnu.org/licenses/>.
+import sys
+
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
 
